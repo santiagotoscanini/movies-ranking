a28f1629c3009fb25de665ab1919a5be7e11b320
Sofia Tejerina <sofiatejerina23@gmail.com>
2020-03-02 16:39:54 -0300

    Add form layout

---
+++ js/initial_page.js
@@ -0,0 +1,9 @@
+autofitIframe = (id) => {
+    if (!window.opera && document.all && document.getElementById) {
+        id.style.height = id.contentWindow.document.body.scrollHeight;
+    } else if (document.getElementById) {
+        id.style.height = id.contentDocument.body.scrollHeight + "px";
+    }
+}
+
+
